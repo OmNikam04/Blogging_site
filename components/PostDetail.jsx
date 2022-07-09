3435d60a8bb5215588f3562fe8c00d6300b2fbe3
Om Nikam <omnikam0404@gmail.com>
2022-07-10 00:21:52 +0530

    postDetails changed

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -5,43 +5,43 @@ import { RichText } from '@graphcms/rich-text-react-renderer'
 const PostDetail = ({ post }) => {
 
   // ! instead of using this code from jsmastery you used the rich-text-react-renderer package
-  // const getContentFragment = (index, text, obj, type) => {
-  //   let modifiedText = text;
+  const getContentFragment = (index, text, obj, type) => {
+    let modifiedText = text;
 
-  //   if (obj) {
-  //     if (obj.bold) {
-  //       modifiedText = (<b key={index}>{text}</b>);
-  //     }
+    if (obj) {
+      if (obj.bold) {
+        modifiedText = (<b key={index}>{text}</b>);
+      }
 
-  //     if (obj.italic) {
-  //       modifiedText = (<em key={index}>{text}</em>);
-  //     }
+      if (obj.italic) {
+        modifiedText = (<em key={index}>{text}</em>);
+      }
 
-  //     if (obj.underline) {
-  //       modifiedText = (<u key={index}>{text}</u>);
-  //     }
-  //   }
-  //   switch (type) {
-  //     case 'heading-three':
-  //       return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
-  //     case 'paragraph':
-  //       return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
-  //     case 'heading-four':
-  //       return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
-  //     case 'image':
-  //       return (
-  //         <img
-  //           key={index}
-  //           alt={obj.title}
-  //           height={obj.height}
-  //           width={obj.width}
-  //           src={obj.src}
-  //         />
-  //       );
-  //     default:
-  //       return modifiedText;
-  //   }
-  // };
+      if (obj.underline) {
+        modifiedText = (<u key={index}>{text}</u>);
+      }
+    }
+    switch (type) {
+      case 'heading-three':
+        return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+      case 'paragraph':
+        return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+      case 'heading-four':
+        return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+      case 'image':
+        return (
+          <img
+            key={index}
+            alt={obj.title}
+            height={obj.height}
+            width={obj.width}
+            src={obj.src}
+          />
+        );
+      default:
+        return modifiedText;
+    }
+  };
 
   return (
     <div className="mb-8 rounded-lg bg-white pb-12 shadow-lg lg:p-8">
@@ -87,12 +87,12 @@ const PostDetail = ({ post }) => {
           </div>
         </div>
         <h1 className='mb-8 text-3xl font-semibold'>{post.title}</h1>
-        {/* {console.log(post.content.raw)} */}
-        {/* {post.content.raw.children.map((typeObj, index)=>{
+        {console.log(post.content.raw)}
+        {post.content.raw.children.map((typeObj, index)=>{
           const children = typeObj.children.map((item, itemIndex)=> getContentFragment(itemIndex, item.text, item))
           return getContentFragment(index, children, typeObj, typeObj.type)
-        })} */}
-        <RichText content={post.content.raw}/>
+        })}
+        {/* <RichText content={post.content.raw}/> */}
       </div>
     </div>
   )
